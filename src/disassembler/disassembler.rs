8124a8972be4f9c12d2460bcbe7a98556a4c941a
Kevin Darlington <kevin@outroot.com>
2016-07-30 01:29:47 -0400

    Got rid of pattern matching when testing opcodes.
    It was getting hard to determine what was what at a glance. It's
    more verbose but easier to just list each opcode or range.

--- src/disassembler/disassembler.rs
+++ src/disassembler/disassembler.rs
@@ -47,72 +47,83 @@ impl Disassembler {
         }
       }
     } else {
-      if op & 0b11000111 == 0b00000100 {
-        let r = op >> 3 & 0b111;
-        Some((Instruction::INC_r(Reg::from(r)), pc))
-      } else if op & 0b11111000 == 0b01110000 {
-        let r = op & 0b111;
-        Some((Instruction::LD_·HL·_r(Reg::from(r)), pc))
-      } else if op & 0b11001111 == 0b00000001 {
-        let r = op >> 4 & 0b11;
-        let nn = try_o!(m.read_word(addr + pc));
-        pc += 2;
-        Some((Instruction::LD_dd_nn(Reg::from_pair(r), nn), pc))
-      } else if op & 0b11000111 == 0b00000110 {
-        let r = op >> 3 & 0b111;
-        let n = try_o!(m.read_byte(addr + pc));
-        pc += 1;
-        Some((Instruction::LD_r_n(Reg::from(r), n), pc))
-      } else if op & 0b11000000 == 0b01000000 {
-        let r1 = op >> 3 & 0b111;
-        let r2 = op & 0b111;
-        Some((Instruction::LD_r_r(Reg::from(r1), Reg::from(r2)), pc))
-      } else if op & 0b11001111 == 0b00000011 {
-        let ss = op >> 4 & 0b11;
-        Some((Instruction::INC_rr(Reg::from_pair(ss)), pc))
-      } else {
-        match op {
-          0x20 => {
-            let e = try_o!(m.read_byte(addr + pc));
-
-            pc += 1;
-            Some((Instruction::JR_cc_e(Flag::NZ, e as i8), pc))
-          }
-          0x28 => {
-            let e = try_o!(m.read_byte(addr + pc));
-            pc += 1;
-            Some((Instruction::JR_cc_e(Flag::Z, e as i8), pc))
-          }
-          0x30 => {
-            let e = try_o!(m.read_byte(addr + pc));
-            pc += 1;
-            Some((Instruction::JR_cc_e(Flag::NC, e as i8), pc))
-          }
-          0x38 => {
-            let e = try_o!(m.read_byte(addr + pc));
-            pc += 1;
-            Some((Instruction::JR_cc_e(Flag::C, e as i8), pc))
-          }
-          0xCD => {
-            let nn = try_o!(m.read_word(addr + pc));
-            pc += 2;
-            Some((Instruction::CALL_nn(nn), pc))
-          }
-          0xE2 => Some((Instruction::LD_0xFF00C_A, pc)),
-          0xE0 => Some((Instruction::LD_0xFF00n_A, pc)),
-          0x1A => Some((Instruction::LD_A_·DE·, pc)),
-          0x32 => Some((Instruction::LDD_·HL·_A, pc)),
-          0x22 => Some((Instruction::LDI_·HL·_A, pc)),
-          0x00 => Some((Instruction::NOP, pc)),
-          0xAF => Some((Instruction::XOR_r(Reg::A), pc)),
-          0xFE => {
-            let n = try_o!(m.read_byte(addr + pc));
-            pc += 1;
-            Some((Instruction::CP_n(n), pc))
-          }
-
-          _ => panic!("instruction_at: instruction not implemented: 0x{:02x}", op),
+      match op {
+        0x4 | 0xc | 0x14 | 0x1c | 0x24 | 0x2c | 0x3c => {
+          let r = op >> 3 & 0b111;
+          Some((Instruction::INC_r(Reg::from(r)), pc))
         }
+
+        0x70...0x75 | 0x77 => {
+          let r = op & 0b111;
+          Some((Instruction::LD_·HL·_r(Reg::from(r)), pc))
+        }
+
+        0x1 | 0x11 | 0x21 | 0x31 => {
+          let r = op >> 4 & 0b11;
+          let nn = try_o!(m.read_word(addr + pc));
+          pc += 2;
+          Some((Instruction::LD_dd_nn(Reg::from_pair(r), nn), pc))
+        }
+
+        0x6 | 0xe | 0x16 | 0x1e | 0x26 | 0x2e | 0x3e => {
+          let r = op >> 3 & 0b111;
+          let n = try_o!(m.read_byte(addr + pc));
+          pc += 1;
+          Some((Instruction::LD_r_n(Reg::from(r), n), pc))
+        }
+
+        0x40...0x45 | 0x47...0x4D | 0x4F...0x55 | 0x57...0x5D | 0x5F...0x65 | 0x67...0x6D |
+        0x6F | 0x78...0x7D | 0x7F => {
+          let r1 = op >> 3 & 0b111;
+          let r2 = op & 0b111;
+          Some((Instruction::LD_r_r(Reg::from(r1), Reg::from(r2)), pc))
+        }
+
+        0x3 | 0x13 | 0x23 | 0x33 => {
+          let ss = op >> 4 & 0b11;
+          Some((Instruction::INC_rr(Reg::from_pair(ss)), pc))
+        }
+
+        0x20 => {
+          let e = try_o!(m.read_byte(addr + pc));
+
+          pc += 1;
+          Some((Instruction::JR_cc_e(Flag::NZ, e as i8), pc))
+        }
+        0x28 => {
+          let e = try_o!(m.read_byte(addr + pc));
+          pc += 1;
+          Some((Instruction::JR_cc_e(Flag::Z, e as i8), pc))
+        }
+        0x30 => {
+          let e = try_o!(m.read_byte(addr + pc));
+          pc += 1;
+          Some((Instruction::JR_cc_e(Flag::NC, e as i8), pc))
+        }
+        0x38 => {
+          let e = try_o!(m.read_byte(addr + pc));
+          pc += 1;
+          Some((Instruction::JR_cc_e(Flag::C, e as i8), pc))
+        }
+        0xCD => {
+          let nn = try_o!(m.read_word(addr + pc));
+          pc += 2;
+          Some((Instruction::CALL_nn(nn), pc))
+        }
+        0xE2 => Some((Instruction::LD_0xFF00C_A, pc)),
+        0xE0 => Some((Instruction::LD_0xFF00n_A, pc)),
+        0x1A => Some((Instruction::LD_A_·DE·, pc)),
+        0x32 => Some((Instruction::LDD_·HL·_A, pc)),
+        0x22 => Some((Instruction::LDD_·HL·_A, pc)),
+        0x00 => Some((Instruction::NOP, pc)),
+        0xAF => Some((Instruction::XOR_r(Reg::A), pc)),
+        0xFE => {
+          let n = try_o!(m.read_byte(addr + pc));
+          pc += 1;
+          Some((Instruction::CP_n(n), pc))
+        }
+
+        _ => panic!("instruction_at: instruction not implemented: 0x{:02x}", op),
       }
     }
   }
