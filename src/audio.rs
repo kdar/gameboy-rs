80f5667f989d1920865e57b902cdf8f6d333327c
Kevin Darlington <kevin@outroot.com>
2016-08-03 21:10:29 -0400

    Removed print statements from audio temporarily.

--- src/audio.rs
+++ src/audio.rs
@@ -16,7 +16,7 @@ impl Audio {
 
 impl MemoryIo for Audio {
   fn read_byte(&self, addr: u16) -> Result<u8, String> {
-    println!("reading audio byte from: {:#04x}", addr);
+    // println!("reading audio byte from: {:#04x}", addr);
     match addr {
       0xFF44 => Ok(0x90), // Some(self.current_line),
       _ => Ok(0),
@@ -24,7 +24,7 @@ impl MemoryIo for Audio {
   }
 
   fn write_byte(&mut self, addr: u16, value: u8) -> Result<(), String> {
-    println!("write audio byte to: {:#04x} {}", addr, value);
+    // println!("write audio byte to: {:#04x} {}", addr, value);
     Ok(())
   }
 }
