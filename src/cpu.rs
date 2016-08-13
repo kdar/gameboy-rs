fd33943386e2dd7dee3dcc0f6fdf6ed9127a460b
Kevin Darlington <kevin@outroot.com>
2016-08-13 16:13:55 -0400

    Fixed ADD_HL instruction to properly implement the carry.

--- src/cpu.rs
+++ src/cpu.rs
@@ -832,7 +832,7 @@ impl Cpu {
   #[allow(non_snake_case)]
   fn inst_ADD_SP(&mut self, o: Operand) {
     let val1 = self.read_reg_u16(Reg::SP);
-    let val2 = self.read_operand_u8(o) as i8 as i16 as u16;
+    let val2 = self.read_operand_u8(o) as i8 as u16;
     let carry = (((val1 & 0xFF) + (val2 & 0xFF)) & 0x100) > 0;
 
     let result = val1.wrapping_add(val2);
@@ -1117,7 +1117,8 @@ impl Cpu {
   fn inst_LD_HL(&mut self, o1: Operand, o2: Operand) {
     let val1 = self.read_operand_u16(o1);
     let val2 = self.read_operand_u8(o2) as i8 as u16;
-    let (result, carry) = val1.overflowing_add(val2);
+    let carry = (((val1 & 0xFF) + (val2 & 0xFF)) & 0x100) > 0;
+    let result = val1.wrapping_add(val2);
 
     self.write_reg_u16(Reg::HL, result);
     self.write_flag(Flag::Z, false);
