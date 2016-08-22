dbaed1bcebfed62536079e6fcdd470cdee6815ec
Kevin Darlington <kevin@outroot.com>
2016-08-21 23:10:19 -0400

    Timer fixed! Code now passes all of Blargg's cpu instrs tests.

--- src/timer.rs
+++ src/timer.rs
@@ -2,15 +2,22 @@ use super::mem::MemoryIo;
 use super::pic::{Pic, Interrupt};
 
 #[derive(Copy, Clone)]
-enum Divider {
-  // Timer clock is 4096 Hz
-  Div1024 = 10,
-  // Timer clock is 262144 Hz
-  Div16 = 4,
-  // Timer clock is 65536 Hz
-  Div64 = 6,
-  // Timer clock is 16386
-  Div256 = 8,
+enum ClockSpeed {
+  Clock4096hz = 0,
+  Clock262144z = 1,
+  Clock65536z = 2,
+  Clock16384z = 3,
+}
+
+impl ClockSpeed {
+  fn cycles(&self) -> i32 {
+    match *self {
+      ClockSpeed::Clock4096hz => 256,
+      ClockSpeed::Clock262144z => 4,
+      ClockSpeed::Clock65536z => 16,
+      ClockSpeed::Clock16384z => 64,
+    }
+  }
 }
 
 pub struct Timer {
@@ -19,9 +26,9 @@ pub struct Timer {
   reg_modulo: u8, // TMA
   reg_control: u8, // TAC
 
-  counter: u32,
-  divider: Divider,
+  clock_speed: ClockSpeed,
   enabled: bool,
+  cycles: i32,
 }
 
 impl Default for Timer {
@@ -31,9 +38,9 @@ impl Default for Timer {
       reg_counter: 0,
       reg_modulo: 0,
       reg_control: 0,
-      counter: 0,
-      divider: Divider::Div1024,
+      clock_speed: ClockSpeed::Clock4096hz,
       enabled: false,
+      cycles: ClockSpeed::Clock4096hz.cycles(),
     }
   }
 }
@@ -56,14 +63,14 @@ impl MemoryIo for Timer {
       0xff05 => self.reg_counter = value,
       0xff06 => self.reg_modulo = value,
       0xff07 => {
-        self.divider = match value & 0x3 {
-          0 => Divider::Div1024,
-          1 => Divider::Div16,
-          2 => Divider::Div64,
-          3 => Divider::Div256,
+        self.clock_speed = match value & 0x3 {
+          0 => ClockSpeed::Clock4096hz,
+          1 => ClockSpeed::Clock262144z,
+          2 => ClockSpeed::Clock65536z,
+          3 => ClockSpeed::Clock16384z,
           _ => unreachable!(),
         };
-
+        self.cycles = self.clock_speed.cycles();
         self.enabled = value & 0x4 != 0;
         self.reg_control = value & 0x7;
       }
@@ -75,19 +82,27 @@ impl MemoryIo for Timer {
 
 impl Timer {
   pub fn step(&mut self, pic: &mut Pic) {
-    self.counter += 1;
-
     if !self.enabled {
       return;
     }
 
-    let mask = (1 << (self.divider as usize)) - 1;
+    // Each step we decrease the cycle.
+    self.cycles -= 1;
 
-    if self.counter & mask == 0 {
-      self.reg_counter = self.reg_counter.wrapping_add(1);
-      if self.reg_counter == 0 {
+    // If we are 0 or less, then we need to add the cycles back
+    // based on the clock speed, and then determine if we've overflowed
+    // or not.
+    if self.cycles <= 0 {
+      self.cycles += self.clock_speed.cycles();
+      // The counter register overflowed! The docs say we must
+      // copy the modulo register to the counter register, and
+      // trigger an interrupt.
+      if self.reg_counter == 0xff {
         self.reg_counter = self.reg_modulo;
         pic.interrupt(Interrupt::Timer);
+      } else {
+        // No overflow. Just increase the counter register.
+        self.reg_counter += 1;
       }
     }
   }
