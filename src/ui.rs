c424ca2e046fac803623858267974e28925d06a9
Kevin Darlington <kevin@outroot.com>
2016-08-26 03:11:59 -0400

    Fixed lints in ui.

--- src/ui.rs
+++ src/ui.rs
@@ -36,7 +36,8 @@ impl Ui {
     let mut u = Ui { event_receiver: r, ..Ui::default() };
 
     u.win = Some(WindowSettings::new("Gameboy-rs",
-                                     [((u.width as f64) * u.scale) as u32, ((u.height as f64) * u.scale) as u32])
+                                     [((u.width as f64) * u.scale) as u32,
+                                      ((u.height as f64) * u.scale) as u32])
       .exit_on_esc(true)
       .build()
       .unwrap());
@@ -47,28 +48,27 @@ impl Ui {
     let mut win = self.win.as_mut().unwrap();
     let image = Image::new().rect([0.0, 0.0, self.width as f64, self.height as f64]);
 
-    let mut texture = Texture::from_image(&mut win.factory, &self.buffer, &TextureSettings::new()).unwrap();
+    let mut texture = Texture::from_image(&mut win.factory, &self.buffer, &TextureSettings::new())
+      .unwrap();
 
     let mut frame_count = 0;
     let mut start = SteadyTime::now();
     while let Some(e) = win.next() {
       frame_count += 1;
 
-      if let Ok(e) = self.event_receiver.try_recv() {
-        match e {
-          GbEvent::Frame(d) => {
-            for (x, y, pixel) in self.buffer.enumerate_pixels_mut() {
-              let v = d[(y as usize) * (self.width as usize) + (x as usize)];
-              // let (r, g, b, a) = ((v >> 24 & 0xff) as u8, (v >> 16 & 0xff) as u8, (v >> 8 & 0xff) as u8, (v & 0xff) as u8);
-              *pixel = im::Rgba([v[0], v[1], v[2], v[3]])
-            }
-            // im::imageops::resize(&self.buffer,
-            //                     ((self.width as f64) * self.scale) as u32,
-            //                     ((self.height as f64) * self.scale) as u32,
-            //                     im::FilterType::CatmullRom);
-            self.doflush = true;
-          }
+      if let Ok(evt) = self.event_receiver.try_recv() {
+        let GbEvent::Frame(d) = evt;
+        for (x, y, pixel) in self.buffer.enumerate_pixels_mut() {
+          let v = d[(y as usize) * (self.width as usize) + (x as usize)];
+          // let (r, g, b, a) = ((v >> 24 & 0xff) as u8,
+          // (v >> 16 & 0xff) as u8, (v >> 8 & 0xff) as u8, (v & 0xff) as u8);
+          *pixel = im::Rgba([v[0], v[1], v[2], v[3]])
         }
+        // im::imageops::resize(&self.buffer,
+        //                     ((self.width as f64) * self.scale) as u32,
+        //                     ((self.height as f64) * self.scale) as u32,
+        //                     im::FilterType::CatmullRom);
+        self.doflush = true;
       }
 
       if SteadyTime::now() - start >= Duration::seconds(1) {
@@ -77,24 +77,20 @@ impl Ui {
         start = SteadyTime::now();
       }
 
-      match e {
-        Event::Render(_) => {
-          if self.doflush {
-            texture.update(&mut win.encoder, &self.buffer).unwrap();
-            let scale = self.scale;
-            win.draw_2d(&e, |c, g| {
-              clear([0.0; 4], g);
-              image.draw(&texture,
-                         &draw_state::DrawState::default(),
-                         c.zoom(scale).transform,
-                         g);
-            });
-            self.doflush = false;
-          }
+      if let Event::Render(_) = e {
+        if self.doflush {
+          texture.update(&mut win.encoder, &self.buffer).unwrap();
+          let scale = self.scale;
+          win.draw_2d(&e, |c, g| {
+            clear([0.0; 4], g);
+            image.draw(&texture,
+                       &draw_state::DrawState::default(),
+                       c.zoom(scale).transform,
+                       g);
+          });
+          self.doflush = false;
         }
-        Event::Update(_) => {}
-        _ => {}
-      };
+      }
     }
   }
 }
