69de6e71fb36b73f73030c43319ca94a459a721e
Kevin Darlington <kevin@outroot.com>
2016-08-26 13:59:35 -0400

    Fixed renderer to scale pixel art better.

--- src/ui.rs
+++ src/ui.rs
@@ -11,7 +11,6 @@ pub struct Ui {
   scale: f64,
   width: u32,
   height: u32,
-  buffer: im::ImageBuffer<im::Rgba<u8>, Vec<u8>>,
   doflush: bool,
   event_receiver: Receiver<GbEvent>,
 }
@@ -19,12 +18,12 @@ pub struct Ui {
 impl Default for Ui {
   fn default() -> Ui {
     let (_, null_receiver) = mpsc::channel();
+    let scale = 4.0;
     Ui {
       win: None,
-      scale: 4.0,
+      scale: scale,
       width: video::SCREEN_WIDTH,
       height: video::SCREEN_HEIGHT,
-      buffer: im::ImageBuffer::new(video::SCREEN_WIDTH, video::SCREEN_HEIGHT),
       doflush: false,
       event_receiver: null_receiver,
     }
@@ -46,10 +45,7 @@ impl Ui {
 
   pub fn run(&mut self) {
     let mut win = self.win.as_mut().unwrap();
-    let image = Image::new().rect([0.0, 0.0, self.width as f64, self.height as f64]);
-
-    let mut texture = Texture::from_image(&mut win.factory, &self.buffer, &TextureSettings::new())
-      .unwrap();
+    let mut data = vec![];
 
     let mut frame_count = 0;
     let mut start = SteadyTime::now();
@@ -58,16 +54,7 @@ impl Ui {
 
       if let Ok(evt) = self.event_receiver.try_recv() {
         let GbEvent::Frame(d) = evt;
-        for (x, y, pixel) in self.buffer.enumerate_pixels_mut() {
-          let v = d[(y as usize) * (self.width as usize) + (x as usize)];
-          // let (r, g, b, a) = ((v >> 24 & 0xff) as u8,
-          // (v >> 16 & 0xff) as u8, (v >> 8 & 0xff) as u8, (v & 0xff) as u8);
-          *pixel = im::Rgba([v[0], v[1], v[2], v[3]])
-        }
-        // im::imageops::resize(&self.buffer,
-        //                     ((self.width as f64) * self.scale) as u32,
-        //                     ((self.height as f64) * self.scale) as u32,
-        //                     im::FilterType::CatmullRom);
+        data = d;
         self.doflush = true;
       }
 
@@ -79,14 +66,22 @@ impl Ui {
 
       if let Event::Render(_) = e {
         if self.doflush {
-          texture.update(&mut win.encoder, &self.buffer).unwrap();
           let scale = self.scale;
           win.draw_2d(&e, |c, g| {
             clear([0.0; 4], g);
-            image.draw(&texture,
-                       &draw_state::DrawState::default(),
-                       c.zoom(scale).transform,
-                       g);
+
+            for (i, d) in data.iter().enumerate() {
+              let x = i % video::SCREEN_WIDTH as usize;
+              let y = (i - x) / video::SCREEN_WIDTH as usize;
+              Rectangle::new([d[0] as f32 / 255.0,
+                              d[1] as f32 / 255.0,
+                              d[2] as f32 / 255.0,
+                              d[3] as f32 / 255.0])
+                .draw([x as f64 * scale, y as f64 * scale, scale, scale],
+                      &c.draw_state,
+                      c.transform,
+                      g);
+            }
           });
           self.doflush = false;
         }
