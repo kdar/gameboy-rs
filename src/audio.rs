de55c88dc98de6a77700a76950152e75ae33073a
Kevin Darlington <kevin@outroot.com>
2016-08-26 03:06:05 -0400

    Fixed lints in audio.

--- src/audio.rs
+++ src/audio.rs
@@ -18,12 +18,12 @@ impl MemoryIo for Audio {
   fn read_u8(&self, addr: u16) -> Result<u8, String> {
     // println!("reading audio byte from: {:#04x}", addr);
     match addr {
-      0xff10...0xff3f => Ok(0),
+      // 0xff10...0xff3f => Ok(0),
       _ => Ok(0),
     }
   }
 
-  fn write_u8(&mut self, addr: u16, value: u8) -> Result<(), String> {
+  fn write_u8(&mut self, _: u16, _: u8) -> Result<(), String> {
     // println!("write audio byte to: {:#04x} {}", addr, value);
     Ok(())
   }
