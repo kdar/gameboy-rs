9199dd5b1b3a9fc8eb06cb2492ac86ecfcf0ebc6
Kevin Darlington <kevin@outroot.com>
2016-09-09 23:28:00 -0400

    The electron frontend is working!

--- src/frontend-electron/capi.js
+++ src/frontend-electron/capi.js
@@ -5,34 +5,52 @@ var ArrayType = require('ref-array')
 
 var Gameboy = ref.types.void;
 var GameboyPtr = ref.refType(Gameboy);
-var ImageBuffer = ArrayType('uint8');
 
 var lib = ffi.Library(path.join(__dirname, '../../target/debug/libgameboy.so'), {
-  gameboy_new: [GameboyPtr, []],
-  gameboy_step: ["void", [GameboyPtr]],
-  gameboy_video_data: ["void", [GameboyPtr, ImageBuffer]],
+  gameboy_new: [GameboyPtr, ["char*"]],
+  gameboy_run_threaded: ["void", [GameboyPtr]],
+  gameboy_set_button: ["void", [GameboyPtr, "uint8", "bool"]],
+  gameboy_updated_frame: ["int", [GameboyPtr, ref.refType(ref.types.char)]],
   gameboy_drop: ["void", [GameboyPtr]],
 });
 
-function Capi() {
-  this.gb = lib.gameboy_new();
-  this.vid_buffer = new ImageBuffer(160*144*4);
-  for (var i = 0; i < 160*144*4; i++) {
-    this.vid_buffer[i] = 0xff;
-  }
+function Capi(cart_path) {
+  var buffer = new Buffer(cart_path.length);
+  buffer.write(cart_path, 0, "utf-8");
+  this.gb = lib.gameboy_new(buffer);
+  
+  this.vid_buffer = new Buffer(160*144*4);
 }
+
+Capi.prototype.run_threaded = function run_threaded() {
+  lib.gameboy_run_threaded(this.gb);
+};
+
+Capi.prototype.set_button = function set_button(btn, pressed) {
+  var btn_i = 0;
+  switch (btn) {
+    case "right": case 68: btn_i = 1; break;
+    case "left": case 65: btn_i = 2; break;
+    case "up": case 87: btn_i = 4; break;
+    case "down": case 83: btn_i = 8; break;
+    case "a": case 32: btn_i = 16; break;
+    case "b": case 17: btn_i = 32; break;
+    case "select": case 16: btn_i = 64; break;
+    case "start": case 13: btn_i = 128; break;
+  }
+
+  lib.gameboy_set_button(this.gb, btn_i, pressed);
+};
+
+Capi.prototype.updated_frame = function updated_frame() {
+  if (lib.gameboy_updated_frame(this.gb, this.vid_buffer) == 1) {
+    return this.vid_buffer;
+  }
+  return null;
+};
 
 Capi.prototype.drop = function drop() {
   lib.gameboy_drop(this.gb);
 };
 
-Capi.prototype.step = function step() {
-  lib.gameboy_step(this.gb);
-};
-
-Capi.prototype.video_data = function video_data() {
-  lib.gameboy_video_data(this.gb, this.vid_buffer);
-  return this.vid_buffer;
-};
-
-module.exports = new Capi();
+module.exports = Capi;
--- src/frontend-electron/renderer.js
+++ src/frontend-electron/renderer.js
@@ -1,33 +1,39 @@
 const {ipcRenderer} = require('electron');
-const capi = require('./capi');
+const Capi = require('./capi');
+const capi = new Capi("../../res/opus5.gb");
+
+capi.run_threaded();
+
+const back_canvas = document.createElement("canvas");
+back_canvas.width = 160;
+back_canvas.height = 144;
+// document.body.appendChild(back_canvas);   
+const back_context = back_canvas.getContext("2d");
+const back_screen = back_context.createImageData(160, 144);
+back_context.putImageData(back_screen, 0, 0);
 
 const canvas = document.querySelector("#canvas");
-console.log(canvas);
+canvas.width = 160*2;
+canvas.height = 144*2;
 const context = canvas.getContext('2d');
-const screen = context.createImageData(160, 144);
+context.imageSmoothingEnabled = false;
 
-for(i=0; i<screen.data.length; i++) {
-  screen.data[i]=145;
-}
-
-context.putImageData(screen, 0, 0);
+document.addEventListener('keydown', function (e) {
+   capi.set_button(e.which, true);
+});
+document.addEventListener('keyup', function (e) {
+   capi.set_button(e.which, false);
+});
 
 setInterval(function() {
-  const data = capi.video_data();
-  screen.data = data;
-  context.putImageData(screen, 0, 0);
-}, 1000);
+  const data = capi.updated_frame();
+  if (data != null) {
+    for (var x = 0; x < data.length; x++) {
+      back_screen.data[x] = data[x];
+    }
+  }
+  back_context.putImageData(back_screen, 0, 0);
 
-var hrstart = process.hrtime();
-var hz = 0;
-setInterval(function() {   
-  capi.step();
-  // hz++;
-  // const hrend = process.hrtime(hrstart);
-  // if (hrend[0] >= 1) {
-  //   console.log(hz);
-  //   hz = 0;
-  //   hrstart = process.hrtime();
-  // }
-  //console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1]/1000000);
-}, 0);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(back_canvas, 0, 0, 160, 144, 0, 0, 160*2, 144*2);
+}, 1000/60);
